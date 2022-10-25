5bb09d684f4f7fb3575294a9814af293994725f2
Kushal Shakya <83382379+Kushalshakya@users.noreply.github.com>
2022-10-25 16:17:46 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library DB</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="logo.jpg">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header>
+        <div id="header">
+            <div class="logo">
+                <img src="images/logo.jpg">
+            </div>
+            <div class="content">
+                <ul>
+                    <li><a href="index.php" class="on">Home</a></li>
+                    <li><a href="">Books</a></li>
+                    <li><a href="student_login.php">Student_Login</a>
+                        <ul>
+                            <li>
+                                <a href="registration.php">
+                                    Create Account
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="">Admin_login</a></li>
+                    <li><a href="">FeedBack</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <section id="main">
+        <div class="main-content">
+            <h1>Welcome To the library</h1>
+            <h2 class="mid">Opens : 6:00 A.M</h2>
+            <h2>Closes : 6:00 P.M</h2>
+        </div>
+    </section>
+    <footer>
+        <div id="footer">
+            <h1>Need Any help?</h1>
+            <h1>Contact us at : librariansupervisor@gmail.com</h1>
+            <ul>
+                <li><i class="fa fa-facebook"></i></li>
+                <li><i class="fa fa-instagram"></i></li>
+                <li><i class="fa fa-twitter"></i></li>
+                <li><i class="fa fa-vk"></i></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
