ee261cfe5062a088aaa4d0f76ad10ff2f85585c1
Kushal Shakya <83382379+Kushalshakya@users.noreply.github.com>
2022-10-26 10:44:47 +0545

    Changes

--- registration.php
+++ registration.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Page</title>
+    <link rel="icon" href="logo.jpg">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
 </head>
 <body>
     <header>
@@ -37,6 +39,7 @@
             <div class="registration_base">
                 <h1>Library Management System</h1>
                 <h1>Student SignUp Form</h1><br>
+                <input type="text" placeholder="Badge ID" name="badgeid" autocomplete="off" required spellcheck="false"><br>
                 <input type="text" placeholder="Username" name="username" autocomplete="off" required spellcheck="false"><br>
                 <input type="number" placeholder="RollNo" name="rollno" autocomplete="off"><br>
                 <input type="password" placeholder="Password" name="password" autocomplete="off" required spellcheck="false"><br>
@@ -51,7 +54,7 @@
         $db = mysqli_connect("localhost","root","","weblibrary");
 
         if(isset($_POST['login'])){
-            if(mysqli_query($db, "INSERT INTO `data` VALUES ('$_POST[username]', '$_POST[password]');")){
+            if(mysqli_query($db, "INSERT INTO `data` VALUES ('$_POST[badgeid]','$_POST[username]', '$_POST[password]');")){
                 header('location:./student_login.php');
                 echo '<script>alert("Registration Successful. Please Login")</script>';
             }
--- student_login.php
+++ student_login.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student_Login Page</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="logo.jpg">
+    <link rel="stylesheet" href="responsive.css">
 </head>
 <body>
     <header>
@@ -37,6 +39,7 @@
             <div class="login_base">
                 <h1>Library Management System</h1>
                 <h1>Student Login Form</h1><br>
+                <input type="text" placeholder="Badge ID" name="badgeid" autocomplete="off" required spellcheck="false"><br>
                 <input type="text" placeholder="Username" name="username" autocomplete="off" required spellcheck="false"><br>
                 <input type="password" placeholder="Password" name="password" autocomplete="off" required spellcheck="false"><br>
                 <button name="login">Login</button>
@@ -49,5 +52,22 @@
             </div>
         </section>
     </form>
+    <?php
+        include "connection.php";
+        if(isset($_POST['login'])){
+            $count = 0;
+            $res = mysqli_query($db, "SELECT * FROM `data` WHERE badgeid='$_POST[badgeid]' && username='$_POST[username]' && password='$_POST[password]';");
+            $count = mysqli_num_rows($res);
+            if($count == 0){
+            echo'<script>alert("Login Credentials Doesnot match.")</script>';
+            }
+            else{
+                if($res){
+                    header('location:./index.php');
+                    echo '<script>alert("Login Successful")</script>';
+                }
+            }
+        }
+    ?>
 </body>
 </html>
